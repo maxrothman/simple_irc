4e8dcfe082972e93d08c5dbfee5a8d7869c9e5f4
whereswalden90 <whereswalden90@gmail.com>
2014-11-14 10:22:41 -0500

    Got it uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 import simple_irc
 setup(
   name = 'simple_irc',
-  packages = ['simple_irc'],
+  py_modules = ['simple_irc'],
   version = simple_irc.__version__,
   description = 'A simple, Pythonic IRC interface',
   author = 'Max Rothman',
   author_email = 'max.r.rothman@gmail.com',
   url = 'https://github.com/maxrothman/' + simple_irc.__name__,
   download_url = 'https://github.com/maxrothman/simple_irc/tarball/' + simple_irc.__version__,
-  keywords = ['IRC', 'simple'],
-  classifiers = [],
+  keywords = 'simple IRC',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3'
+  ],
 )
